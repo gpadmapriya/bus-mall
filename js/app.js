8a4accb751c928e943a9857a54dcfa105b1d7925
Padmapriya Ganapathi <gpadmapriya@hotmail.com>
2019-06-17 19:23:08 -0700

    rendering 3 images

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 'use strict';
 
+console.log('in JS');
 var leftProduct = null;
+var centerProduct = null;
 var rightProduct = null;
+
+var allProductsSectionTag = document.getElementById('all_products');
+var leftProductTag = document.getElementById('left_product_img');
+var rightProductTag = document.getElementById('right_product_img');
+var centerProductTag = document.getElementById('center_product_img');
+
 
 var Products = function (name, imageSrc) {
   this.name = name;
@@ -12,7 +20,7 @@ var Products = function (name, imageSrc) {
   Products.allProducts.push(this);
 };
 
-Products.allImages = [];
+Products.allProducts = [];
 
 new Products('Bag', './img/bag.jpg');
 
@@ -37,4 +45,35 @@ new Products('Boots', './img/usb.gif');
 new Products('Boots', './img/water-can.jpg');
 new Products('Boots', './img/wine-glass.jpg');
 
+console.log('Products ' + Products.allProducts[0]);
 
+
+var pickNextProducts = function () {
+  console.log('picking products');
+  var leftIndex = Math.floor(Math.random() * Products.allProducts.length - 1);
+  var centerIndex = Math.floor(Math.random() * Products.allProducts.length - 1);
+  var rightIndex = Math.floor(Math.random() * Products.allProducts.length - 1);
+
+  /*do {
+    var rightIndex = Math.floor(Math.random() * Products.allImages.length - 1);
+  } while (rightIndex === leftIndex);
+  //console.log(Products.allImages[leftIndex].name, Products.allImages[rightIndex].name);
+
+  do {
+    var centerIndex = Math.floor(Math.random() * Products.allImages.length - 1);
+  } while (rightIndex === leftIndex);
+  leftProduct = Products.allImages[leftIndex];
+  rightProduct = Products.allImages[rightIndex];
+  centerProduct = Products.allImages[centerIndex];*/
+
+  renderNewProducts(leftIndex, centerIndex, rightIndex);
+};
+
+var renderNewProducts = function (leftIndex, centerIndex, rightIndex) {
+  console.log('left product ' + Products.allProducts[leftIndex]);
+  leftProductTag.src = Products.allProducts[leftIndex].url;
+  centerProductTag.src = Products.allProducts[centerIndex].url;
+  rightProductTag.src = Products.allProducts[rightIndex].url;
+};
+
+pickNextProducts();
